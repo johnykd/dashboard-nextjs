ee34ada7301b8f0c5121352a22f7455924024743
pattaranun <pattaranunn@motorist.co.th>
2024-11-05 21:56:32 +0700

    connected DB, fetch data streaming

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,9 @@
+import DashboardSkeleton from "../../ui/skeletons";
+
+export default function Loadings() {
+    return (
+        <div>
+            <DashboardSkeleton />;
+        </div>
+    );
+}
